a3fd7fae34c9e58c2f40558404e40abfe0799c1f
Satish Mishra <zicsxone@gmail.com>
2023-07-16 14:59:46 +0530

    host

--- location_validator/settings.py
+++ location_validator/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '',
+        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
 }
