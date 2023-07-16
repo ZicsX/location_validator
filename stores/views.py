792e2d5a36b33672ee093ac2ad5c4609a6e9fc49
Satish Mishra <zicsxone@gmail.com>
2023-07-16 15:40:35 +0530

    added views

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .forms import UrlForm
+from .models import Store
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = UrlForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+            # scraping
+            # validation
+
+            return render(request, 'stores/result.html')
+    else:
+        form = UrlForm()
+
+    return render(request, 'stores/home.html', {'form': form})
+
+def result(request):
+    all_stores = Store.objects.all()
+    verified_stores = Store.objects.filter(is_verified=True)
+    flagged_stores = Store.objects.filter(is_verified=False)
+
+    context = {
+        'all_stores': all_stores,
+        'verified_stores': verified_stores,
+        'flagged_stores': flagged_stores,
+    }
+
+    return render(request, 'stores/result.html', context)
