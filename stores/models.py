1fce262ba0bf09d8bca96d7a7315dc841fdb064b
Satish Mishra <zicsxone@gmail.com>
2023-07-16 15:39:00 +0530

    added model

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
