cc9a6a7aaba1c3b4f98861435da3041fd0d2dafb
Satish Mishra <zicsxone@gmail.com>
2023-07-16 14:59:04 +0530

    load_dotenv

--- location_validator/settings.py
+++ location_validator/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,7 +69,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': '',
         'PORT': os.getenv('DB_PORT'),
     }
 }
